8bacf4e87703222ef51158b4cd48e03e7c8ed81c
Matt Pellegrini <pelleg@amazon.com>
2017-06-27 17:42:20 +0200

    68 Macros Starting with UPROPERTY

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,34 +1,43 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingEscape.h"
-#include "PositionReport.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+    
+    //Get the owning actor
+    AActor* Owner = GetOwner();
+    
+    //Create a rotator
+    FRotator NewRotation = FRotator(0.0f, -70.0f, 0.0f);
+    
+    //Set the rotation
+    Owner->SetActorRotation(NewRotation);
+    
 
-    FString ObjectName = GetOwner()->GetName();
-    FString ObjectPos = GetOwner()->GetTransform().GetTranslation().ToString();
-    UE_LOG(LogTemp, Warning, TEXT("%s is at position %s"), *ObjectName, *ObjectPos);
+    
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -3,22 +3,29 @@
 #pragma once
 
 #include "Components/ActorComponent.h"
-#include "PositionReport.generated.h"
+#include "OpenDoor.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BUILDINGESCAPE_API UPositionReport : public UActorComponent
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UPositionReport();
+	UOpenDoor();
 
 	// Called when the game starts
 	virtual void BeginPlay() override;
 	
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = 90.0f;
+    
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
 	
 };
